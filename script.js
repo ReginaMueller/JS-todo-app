74537f50d201091081bd35ad0204d556bc02ab0f
Regina Mueller <reginocika.mueller@gmail.com>
2024-04-12 12:32:30 +0200

    added design

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const todo_list = document.getElementById("todo-list");
-const add_button = document.getElementById("add");
-const rmv_button = document.getElementById("remove");
+const add_form = document.getElementById("add-form");
 const all_filter = document.getElementById("all");
 const open_filter = document.getElementById("open");
 const done_filter = document.getElementById("done");
+
 const localStorageKey = "key";
 
 let state = [];
+let filter = all_filter;
 
 function render() {
   todo_list.innerHTML = "";
+  all_filter.style.backgroundColor = "";
+  open_filter.style.backgroundColor = "";
+  done_filter.style.backgroundColor = "";
+  filter.style.backgroundColor = "white";
   updateState();
   visualizeState();
 }
-
-render();
 
 function updateState() {
   if (localStorage.getItem(localStorageKey) !== null)
@@ -27,15 +30,15 @@ function updateLocalStorage() {
 
 function visualizeState() {
   if (state.length != 0) {
-    if (all_filter.checked) {
+    if (filter == all_filter) {
       for (let todo of state) {
         visualize(todo);
       }
-    } else if (open_filter.checked) {
+    } else if (filter == open_filter) {
       for (let todo of state) {
         if (!todo.done) visualize(todo);
       }
-    } else if (done_filter.checked) {
+    } else if (filter == done_filter) {
       for (let todo of state) {
         if (todo.done) visualize(todo);
       }
@@ -44,32 +47,70 @@ function visualizeState() {
 }
 
 function visualize(todoObject) {
-  // Checkbox erstellen und mit EventListener versehen
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.name = "done";
-  checkbox.checked = todoObject.done;
-  checkbox.id = todoObject.id;
-  checkbox.addEventListener("change", (e) => {
-    todoObject.done = e.target.checked;
-    updateLocalStorage();
-  });
   // Span erstellen
   const span = document.createElement("span");
   span.textContent = todoObject.description;
-  // Label erstellen
-  const label = document.createElement("label");
-  // Span und Checkbox in das Label einfügen
-  label.append(span, checkbox);
   //Form erstellen
   const form = document.createElement("form");
   // Label in Form einfügen
-  form.append(label);
-  // List erstellen
-  const list = document.createElement("li");
-  // Form in List einfügen
-  list.append(form);
-  todo_list.appendChild(list);
+  form.append(span);
+  form.checked = todoObject.done;
+  form.id = todoObject.id;
+  styleForm(form);
+  form.addEventListener("click", () => {
+    form.checked = !form.checked;
+    todoObject.done = form.checked;
+    styleForm(form);
+    updateLocalStorage();
+  });
+
+  todo_list.appendChild(form);
+}
+
+function styleForm(form) {
+  if (form.checked) {
+    form.style.border = "0.2rem solid";
+    form.style.borderColor = "white";
+    form.style.backgroundColor = "inherit";
+    form.style.color = "white";
+    const check_square = document.createElement("i");
+    check_square.classList.add("fa-regular", "fa-square-check");
+    const trash = document.createElement("i");
+    trash.classList.add("fa-regular", "fa-trash-can");
+    const h2_check = document.createElement("h2");
+    h2_check.id = form.id + "check";
+    h2_check.appendChild(check_square);
+    h2_check.style.color = "white";
+    const h2_trash = document.createElement("h2");
+    h2_trash.id = form.id + "trash";
+    h2_trash.appendChild(trash);
+    h2_trash.style.color = "white";
+    h2_trash.style.zIndex = "2";
+    h2_trash.hidden = "true";
+    form.append(h2_trash, h2_check);
+    form.addEventListener("mouseover", () => {
+      h2_trash.hidden = "";
+    });
+    form.addEventListener("mouseout", () => {
+      h2_trash.hidden = "true";
+    });
+    trash.addEventListener("click", () => {
+      state = state.filter((todo) => todo.id != form.id);
+      updateLocalStorage();
+      render();
+    });
+  } else {
+    form.style.backgroundColor = "";
+    form.style.color = "";
+    const check = document.getElementById(form.id + "check");
+    if (check != null) {
+      check.parentNode.removeChild(check);
+    }
+    const trash = document.getElementById(form.id + "trash");
+    if (trash != null) {
+      trash.parentNode.removeChild(trash);
+    }
+  }
 }
 
 function createTodoObject(todo_txt) {
@@ -80,7 +121,29 @@ function createTodoObject(todo_txt) {
   return todo;
 }
 
-add_button.addEventListener("click", () => {
+all_filter.addEventListener("click", () => {
+  if (filter != all_filter) {
+    filter = all_filter;
+    render();
+  }
+});
+
+open_filter.addEventListener("click", () => {
+  if (filter != open_filter) {
+    filter = open_filter;
+    render();
+  }
+});
+
+done_filter.addEventListener("click", () => {
+  if (filter != done_filter) {
+    filter = done_filter;
+    render();
+  }
+});
+
+add_form.addEventListener("submit", () => {
+  event.preventDefault();
   const todo_input = document.getElementById("input");
   const todoObject = createTodoObject(todo_input.value.trim());
   visualize(todoObject);
@@ -89,21 +152,4 @@ add_button.addEventListener("click", () => {
   todo_input.value = "";
 });
 
-rmv_button.addEventListener("click", () => {
-  console.log(state);
-  state = state.filter((todo) => !todo.done);
-  updateLocalStorage();
-  render();
-});
-
-all_filter.addEventListener("change", () => {
-  render();
-});
-
-open_filter.addEventListener("change", () => {
-  render();
-});
-
-done_filter.addEventListener("change", () => {
-  render();
-});
+render();
